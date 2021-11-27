d19409ca620e8c14c3b61cb9b6ed5ed34f189fdc
dmitrii.voronin <VoronindimTvink123@mail.ru>
2021-11-27 14:39:27 +0300

    Initial Commit

---
+++ FeatureEmployeesRegister/FeatureEmployeesRegister.h
@@ -0,0 +1,18 @@
+//
+//  FeatureEmployeesRegister.h
+//  FeatureEmployeesRegister
+//
+//  Created by Dmitrii Voronin on 27.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeatureEmployeesRegister.
+FOUNDATION_EXPORT double FeatureEmployeesRegisterVersionNumber;
+
+//! Project version string for FeatureEmployeesRegister.
+FOUNDATION_EXPORT const unsigned char FeatureEmployeesRegisterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureEmployeesRegister/PublicHeader.h>
+
+
